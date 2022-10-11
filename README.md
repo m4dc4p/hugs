19b289e93a7c158bb430ccfeafb0fccc5abf8662
Justin Bailey <justin.bailey@well.co>
2022-10-11 12:01:42 -0700

    Flakifying Hugs
    * Defined a nix flake for building Hugs on x86 Macs.
    * Defined apps for the flake that run each binary provided by Hugs.
    * Documented build process, etc.

---
+++ README.md
@@ -0,0 +1,71 @@
+# Hugs 98 for MacOS (x86)
+
+This flake defines how to build and run the Hugs Haskell interpreter on 
+MacOS machines with the x86 chipset. Its quite likely that this source can
+build on M1, etc. but someone else will have to test that.
+
+This flake builds from the last public source release of Hugs (September 2006). No
+updates were made to the source, except those necessary to get it to compile.
+
+# Building Hugs
+
+With nix installed and flakes enabled, you can build hugs quite easily. From the root
+of this repo:
+
+
+```bash
+$ nix build
+```
+
+The `result` symlink will point to a directory containing `bin`, `hugsdir` and some
+other directories:
+
+* `bin` - Hugs interpreter and other executables (`hugs`, `runhugs`, `ffihugs`, `cpphs-hugs` and `hsc2hs-hugs`)
+* `hugsdir` - This contains all the libraries that shipped with the last release of Hugs.
+
+# Running Hugs
+
+Hugs and friends can be run directly from this flake:
+
+* `nix run` (that is, the default app) will execute `runhugs`, which expects a Haskell source file to execute.
+* `#hugs` - Starts the Hugs interpreter.
+* `#runhugs` - Executes `runhugs` (same as the default)
+* `#ffihugs` - Executes the `ffihugs` program.
+* `#cpphs-hugs` - Executes the `cpphs-hugs` program.
+* `#hsc2hs-hugs` - Executes the `hsc2hs-hugs` program.
+
+## HUGSDIR
+
+`HUGSDIR` is a compile time value that is used when resolving path names while searching for modules. By default,
+it will be set to the absolute path to the `hugsdir` directory in derivation's output (that is, the directory pointed to
+by `result` above).
+
+`man hugs` explains how to override HUGSDIR and how to use it in conjunction with the search path (`-P` option) in
+order to find modules.
+
+# Notes on Buildin Hugs with Nix
+
+Ironically, the hardest part in getting hugs to build under Nix was due to whitespace handling. In the far
+past, someone mixed tabs and spaces throughout the hugs source code (Haskell files, specifically). `gcc` must
+have originally preserved whitespace, but `clang` (now the standard compiler provided by nix) did not. The
+file `patches/sources.patch` mostly updates indentation so hugs can compile its own source files.
+
+I tried very hard to use `cpphs` (provided externally by nixpkgs) rather than patching all these source files, but
+there were just enough special defines and includes that `clang` already knew about that I couldn't quite get it working. 
+
+The C preprocessor provided by `gcc` also behaved slightly differently than `clang`. In particular, Haskell
+pragrams such as `{-# INLINE #-}` could not be split across multiple lines, such as:
+
+    {-# INLINE 
+     #-}
+
+As the preprocess would think the final line was a preprocess directive, and halt.
+
+Some of the scripts in the source distribution seem plain bugged, as they were missing source files
+or preprocessor defines. 
+
+`hsc2hs` (built during bootstrapping) was hardcoded to use `gcc`; not surprising, `clang` didn't even exist
+when this was last released! `gcc` is provided by nix so the build can complete, and is also provided to the `hsc2hs-hugs`
+binary (though I'm not sure what that acutally does).
+
+
--- flake.nix
+++ flake.nix
@@ -2,52 +2,77 @@
   description = "Builds the Hugs98 Interpreter";
 
   inputs.nixpkgs.url = "nixpkgs/release-22.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   nixConfig = {
     bash-prompt-prefix = "hugs >";
   };
 
-  outputs = { self, nixpkgs }:     
-    let 
-      system = "x86_64-darwin";
-      inherit (nixpkgs) lib;
-      inherit (nixpkgs.legacyPackages.${system}) mkShell stdenv;
-      inherit (nixpkgs.legacyPackages.${system}.darwin) Libsystem;
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      systems = [ flake-utils.lib.system.x86_64-darwin ];
+    in
+      flake-utils.lib.eachSystem systems (system: 
+        let 
+          inherit (nixpkgs.legacyPackages.${system}) mkShell stdenv gcc makeWrapper;
 
-      hugsPkg = stdenv.mkDerivation {
-        pname = "hugs98";
-        version = "2006-09";
+          hugsPkg = stdenv.mkDerivation {
+            pname = "hugs98";
+            version = "2006-09";
 
-        patches = [ ./sources.patch ];
+            patches = [ ./patches/sources.patch ];
 
-        src = ./pristine/hugs98-plus-Sep2006.tar.gz;
+            src = ./pristine/hugs98-plus-Sep2006.tar.gz;
 
-        nativeBuildInputs = [ ];
+            nativeBuildInputs = [ gcc makeWrapper ];
 
-        configureFlags = [
-        ];
+            # Set hugsdir so interpreter always finds default
+            # packages.
+            preConfigure = ''
+              export hugsdir=$out/hugsdir;
+            '';
 
-        # Set hugsdir so interpreter always finds default
-        # packages.
-        preConfigure = ''
-          export hugsdir=$out/hugsdir;
-        '';
+            postFixup = ''
+              wrapProgram $out/bin/cpphs-hugs --set PATH $out/bin
+              wrapProgram $out/bin/hsc2hs-hugs --set PATH $out/bin:${gcc}/bin
+            '';
 
-        meta = with lib; {
-          homepage = "https://www.haskell.org/hugs";
-          description = "Haskell interpreter";
-          license = licenses.bsd3;
-          platforms = platforms.all;
-        };
-
-      };
-    in    
-    {
-      packages.x86_64-darwin.default = hugsPkg;
-      devShell.x86_64-darwin = mkShell {
-        inherit (hugsPkg) preConfigure ;
-        packages = [];
-        inputsFrom = [hugsPkg];
-      };
-    };
+            meta = {
+              homepage = "https://www.haskell.org/hugs";
+              description = "Haskell interpreter";
+            };
+          };
+        in    
+          {
+            packages.default = hugsPkg;
+            apps = rec {
+              default = runhugs;
+              hugs = {
+                type = "app";
+                program = "${self.packages.${system}.default}/bin/hugs";
+              };
+              runhugs = {
+                type = "app";
+                program = "${self.packages.${system}.default}/bin/runhugs";
+              };
+              ffihugs = {
+                type = "app";
+                program = "${self.packages.${system}.default}/bin/ffihugs";
+              };
+              cpphs-hugs = {
+                type = "app";
+                program = "${self.packages.${system}.default}/bin/cpphs-hugs";
+              };
+              hsc2hs-hugs = {
+                type = "app";
+                program = "${self.packages.${system}.default}/bin/hsc2hs-hugs";
+              };
+            };
+            devShell = mkShell {
+              inherit (hugsPkg) preConfigure ;
+              packages = [];
+              inputsFrom = [hugsPkg];
+            };
+          }
+        );
 }
